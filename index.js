5403945d314716c8dba71c44cf5af0c13b4b7d20
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-06 07:05:20 +0600

    create dynamic get api for fetch dynamically

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app=express();
-const port= process.env.PORT || 5000;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7nkbk6a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -16,24 +16,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const serviceCollection=client.db("carFixerDB").collection("services");
+    const serviceCollection = client.db("carFixerDB").collection("services");
 
-    app.get("/services",async(req,res)=>{
-        const cursor=serviceCollection.find();
-        const result=await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { title: 1, price: 1, service_id: 1 },
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -41,11 +53,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("car fixer server is running");
+});
 
-app.get("/", (req,res)=>{
-    res.send("car fixer server is running");
-})
-
-app.listen(port,()=>{
-    console.log(`car fixer server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`car fixer server is running on port ${port}`);
+});
