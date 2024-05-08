eaf958767e220b6128ce9b65a37990f53a9b9f79
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-09 02:12:06 +0600

    implement jwt token generation and send to the client cookie and access in browser for authentication

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 const express = require("express");
 const cors = require("cors");
+const jwt =require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:["http://localhost:5173"],
+  credentials:true,
+}));
+app.use(cookieParser())
+
+// MiddleWare >===>
+  const verifyToken=async(req,res,next)=>{
+    const token=req?.cookies?.accessToken;
+    if(!token){
+      return res.status(401).send({message:"unauthorized access"})
+    }
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){
+        res.status(401).send({message:"unauthorized access"})
+      }
+      req.user=decoded;
+      next();
+    })
+  }
+//  const verifyToken=async(req,res,next)=>{
+//   const token=req.cookies?.accessToken;
+//   console.log("value of token in middleware",token);
+//   if(!token){
+//     return res.status(401).send({message:"UnAuthorized"})
+//   }
+//   jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+//     if(err){
+//       console.log(err);
+//       return res.status(401).send({message:"UnAuthorized"})
+//     }
+//     console.log("value in the token",decoded);
+//     req.user=decoded;
+//     next();
+//   })
+//  }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7nkbk6a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -25,6 +62,26 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db("carFixerDB").collection("services");
     const bookingCollection=client.db("carFixerDB").collection("bookedservices")
+
+    app.post("/jwt", async(req,res)=>{
+      const userInfo=req.body;
+      const token=jwt.sign(userInfo,process.env.ACCESS_TOKEN_SECRET,{expiresIn:"2h"})
+      res
+      .cookie("accessToken",token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:"none"
+      })
+      .send({success:true});
+    })
+
+    app.post("/logout",async(req,res)=>{
+      const userInfo=req.body;
+      console.log("logginOut action from cliend side",userInfo);
+      res
+      .clearCookie("accessToken",{maxAge:0})
+      .send({success:true})
+    })
 
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
@@ -50,8 +107,13 @@ async function run() {
       res.send(result);
     })
     
-    app.get("/bookings",async(req,res)=>{
+    app.get("/bookings",verifyToken, async(req,res)=>{
       console.log(req.query.email);
+      console.log(req.user);
+      if(req.user.email !== req.query.email){
+        return
+      }
+      // console.log("Cookies token from client side",req.cookies)
       let query={};
       if(req.query?.email){
         query={email:req.query.email}
@@ -71,7 +133,6 @@ async function run() {
       const id=req.params.id;
       const filter={_id:new ObjectId(id)};
       const updateBooking=req.body;
-      console.log(updateBooking);
       const updatedData={
         $set:{
           status:updateBooking.status
